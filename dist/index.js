1c5fae3a9330eceeb4153406303b45fc052ac95e
AmareshXcool <152835103+AmareshXcool@users.noreply.github.com>
2024-04-11 10:19:25 +0530

    finally-done

--- dist/index.js
+++ dist/index.js
@@ -15,7 +15,14 @@ const app = new hono_1.Hono();
 app.use((0, logger_1.logger)());
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 cloudinary_1.default.v2.config();
-app.use("/api/*", (0, cors_1.cors)());
+app.use("/api/*", (0, cors_1.cors)({
+    origin: process.env.FRONTEND_API || "",
+    allowHeaders: ["Origin", "Content-Type", "Authorization"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+    maxAge: 600,
+    credentials: true,
+}));
 (0, node_server_1.serve)({
     fetch: app.fetch,
     port,
