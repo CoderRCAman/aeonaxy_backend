06a8beccc8bb8eac20e4eee8dc3c68b45187e4d8
AmareshXcool <152835103+AmareshXcool@users.noreply.github.com>
2024-04-11 10:18:52 +0530

    finally-done

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,17 @@ const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 cloudinary.v2.config();
 app.use(
   "/api/*",
-  cors()
+  cors({
+    origin: process.env.FRONTEND_API || "",
+    allowHeaders: ["Origin", "Content-Type", "Authorization"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+    maxAge: 600,
+    credentials: true,
+  })
 );
 serve({
-  fetch: app.fetch, 
+  fetch: app.fetch,
   port,
 });
 console.log("🔥 server running on port:", port);
